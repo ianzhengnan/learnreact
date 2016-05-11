a504203d28359cafed091a24212016592ec9fba6
Ian Zheng <ian.zheng@sap.com>
2016-05-11 18:45:49 +0800

    update

--- www/static/js/example.jsx
+++ www/static/js/example.jsx
@@ -24,12 +24,36 @@ var CommentList = React.createClass({
 
 
 var CommentForm = React.createClass({
-
+	getInitialState() {
+	    return {
+	        author: '', text: ''  
+	    };
+	},
+	handleAuthorChange: function(e){
+		this.setState({
+			author: e.target.value
+		});
+	},
+	handleTextChange: function(e){
+		this.setState({
+			text: e.target.value
+		})
+	},
+	handleSubmit: function(e){
+		e.preventDefault();
+		var author = this.state.author.trim();
+		var text = this.state.text.trim();
+		if (!text || !author) {
+			return;
+		}
+		this.props.onCommentSubmit({author: author, text: text});
+		this.setState({author: '', text: ''});
+	},
 	render: function(){
 		return (
-			<form className="commentForm">
-				<input type="text" placeholder="Your name..." />
-				<input type="text" placeholder="Say something..." />
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+				<input type="text" placeholder="Your name..." value={this.state.author} onChange={this.handleAuthorChange} />
+				<input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
 				<input type="submit" value="Post" />
 			</form>
 		);
@@ -71,6 +95,25 @@ var CommentBox = React.createClass({
 	      	}.bind(this)
 	    });
 	},
+	handleCommentSubmit: function(comment){
+		var comments = this.state.data;
+
+		comment.id = Date.now();
+		var newComments = comments.concat([comment]);
+
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			type: 'POST',
+			data: comment,
+			success: function(data){
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr,status,err){
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
 	getInitialState() {
 	    return {
 	        data: []  
@@ -85,13 +128,13 @@ var CommentBox = React.createClass({
 			<div className="commentBox">
 				<h1>Comments</h1>
 				<CommentList data={this.state.data} />
-				<CommentForm />
+				<CommentForm onCommentSubmit={this.handleCommentSubmit} />
 			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	<CommentBox url="http://ian:5000/api/comments" pollInterval={2000} />,
+	<CommentBox url="/api/comments" pollInterval={2000} />,
 	document.getElementById('content')
 );
