5249b2c24cc11e1ea43c2fdb2eba0d381c41c0d8
Ian Zheng <ian.zheng@sap.com>
2016-05-12 16:10:27 +0800

    update

--- www/app.py
+++ www/app.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask import request, render_template, send_from_directory
 from flask import json
 
+import mysql.connector
+
 
 app = Flask(__name__)
 
@@ -14,20 +16,43 @@ def home():
 @app.route('/api/comments', methods=['GET', 'POST'])
 def handle():
 	if request.method == 'POST':
-		return json.dumps([
-				  {"id": "1", "author": "Pete Hunt", "text": "This is one comment"},
-				  {"id": "2", "author": "Jordan Walke", "text": "This is *another* comment"},
-				  {"id": "3", "author": "Ian Zheng", "text": "This is *my* comment"},
-				  {"id": "4", "author": "kaka", "text": "hello"}
-				])
-
+		try:
+			saveData(request.form['id'], request.form['author'], request.form['text'])
+			return json.dumps({"status": "ok"})
+		except Exception as e:
+			print(e)
 		
 	elif request.method == 'GET':
-		return json.dumps([
-				  {"id": "1", "author": "Pete Hunt", "text": "This is one comment"},
-				  {"id": "2", "author": "Jordan Walke", "text": "This is *another* comment"},
-				  {"id": "3", "author": "Ian Zheng", "text": "This is *my* comment"}
-				])
+		try:
+			conn = mysql.connector.connect(user='root', password='1234567', database='test')
+			cursor = conn.cursor()
+			cursor.execute('select * from comments')
+			values = cursor.fetchall()
+			cursor.close()
+			conn.close()
+		except Exception as e:
+			print(e)
+
+		output = []
+		if values:
+			for item in values:
+				output.append({'id':item[0], 'author': item[1], 'text': item[2]})
+
+		return json.dumps(output) if values else json.dumps()
+
+
+def saveData(id, author, text):
+	try:
+		conn = mysql.connector.connect(user='root', password='1234567', database='test')
+		cursor = conn.cursor()
+		cursor.execute('insert into comments (id, author, text) values (%s, %s, %s)', 
+			[id, author, text])
+		conn.commit()
+		cursor.close()
+		conn.close()
+	except Exception as e:
+		print(e)
+		raise
 
 
 if __name__ == '__main__':
